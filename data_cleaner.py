325e85acc7407c553654bbe32117cdc0d6bd960e
julia-potato <73934921+julia-potato@users.noreply.github.com>
2020-11-18 07:59:16 +0200

    Add files via upload

---
+++ data_cleaner.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+
+def data_cleaner(data):
+    #remove the id, partlybad columns
+    data.drop(["id","partlybad"],1,inplace=True)
+    #set new index using the date column
+    data.set_index(["date"],inplace=True)
+    #add binary 'class2' col for event/non-event
+    data["class2"] = np.where(data["class4"] == "nonevent", "nonevent","event")
+    #arrange to put class2 col at the front
+    cols = ['class2'] + [col for col in data if col != 'class2']
+    data = data[cols]
+    return data

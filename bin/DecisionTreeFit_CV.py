fbb4ff7ea7f036e1849822fe87e7e64a025b3bd4
Bernardo Williams <williwilliams3@hotmail.com>
2020-11-24 15:08:32 -0600

    New data splitting process and multi class DT

--- bin/DecisionTreeFit_CV.py
+++ bin/DecisionTreeFit_CV.py
@@ -8,58 +8,10 @@ Created on Thu Nov 19 20:59:40 2020
 
 import numpy as np
 import pandas as pd
-
-def data_cleaner(df):
-    data =  df.copy()
-    #remove the id, partlybad columns
-    data.drop(["id","partlybad"],1,inplace=True)
-    #set new index using the date column
-    data.set_index(["date"],inplace=True)
-    #add binary 'class2' col for event/non-event
-    data["class2"] = np.where(data["class4"] == "nonevent", "nonevent","event")
-    #arrange to put class2 col at the front
-    cols = ['class2'] + [col for col in data if col != 'class2']
-    data = data[cols]
-    return data
-
-
 import os
-# od.chdir('https://raw.githubusercontent.com/williwilliams3/TermProjectIML/master')
-os.chdir('/Users/bwilliams/GoogleDrive/UniversityOfHelsinki/Fall2020/IML/TeamProject/TermProjectIML/')
 
-#load training data
-url = "data/train.csv"
-npf_train_raw = pd.read_csv(url)
-npf_train = data_cleaner(npf_train_raw)
-
-#create feature array and target column for training data
-X_train = npf_train.drop(["class2","class4"], axis=1)
-y_train = npf_train["class2"]
-
-# load validation data
-url = "data/validation.csv"
-npf_val_raw = pd.read_csv(url)
-npf_val = data_cleaner(npf_val_raw)
-
-#create feature array and target column for validation data
-X_val = npf_train.drop(["class2","class4"], axis=1)
-y_val = npf_train["class2"]
-
-#import test data
-npf_test_raw = pd.read_csv("data/test.csv")
-npf_test = data_cleaner(npf_test_raw)
-
-#create feature array and target column for test data
-X_test = npf_train.drop(["class2","class4"], axis=1)
-y_test = npf_train["class2"]
-
-
-# create complete training set for cross validation with  train +  validation set
-npf_train_val_raw = npf_train_raw.append(npf_val_raw)
-npf_train_val = data_cleaner(npf_train_val_raw)
-X = npf_train.drop(["class2","class4"], axis=1)
-y = npf_train["class2"]
-
+os.chdir('/Users/bwilliams/GoogleDrive/UniversityOfHelsinki/Fall2020/IML/TeamProject/TermProjectIML/bin')
+from SplitData import *
 
 
 #####################################
@@ -67,13 +19,17 @@ y = npf_train["class2"]
 from sklearn import tree
 from sklearn.model_selection import GridSearchCV
 import matplotlib.pyplot as plt
-
+from sklearn import metrics
 
 # https://stackoverflow.com/questions/35097003/cross-validation-decision-trees-in-sklearn
 parameters = {'max_depth':range(1,10)}
 clf = GridSearchCV(tree.DecisionTreeClassifier(random_state=42), parameters, n_jobs=4, cv=10)
-clf.fit(X=X, y=y)
+clf.fit(X=X_trainval, y=y_trainval_bin)
 tree_model = clf.best_estimator_
+
+y_pred_train = tree_model.predict(X_trainval)
+
+print("Accuracy on Train:",metrics.accuracy_score(y_trainval_bin, y_pred_train))
 
 print ('Cross validation Accuracy: ', clf.best_score_, clf.best_params_) 
 
@@ -88,11 +44,9 @@ y_pred = tree_model.predict(X_test)
 # probabilities of class event
 y_pred_proba = tree_model.predict_proba(X_test)[:,0]
 
+print("Accuracy on Test:",metrics.accuracy_score(y_test_bin, y_pred))
 
-from sklearn import metrics
-print("Accuracy on Test:",metrics.accuracy_score(y_test, y_pred))
-
-y_test_01 = pd.get_dummies(y_test)['event']
+y_test_01 = pd.get_dummies(y_test_bin)['event']
 
 fpr, tpr, thresholds = metrics.roc_curve(y_test_01, y_pred_proba)
 metrics.auc(fpr, tpr)
@@ -105,7 +59,7 @@ plt.legend()
 plt.show()
 
 
-# Tree visualizatin
+# Tree visualization
 from sklearn.tree import export_graphviz
 from six import StringIO  
 from IPython.display import Image  
--- bin/SplitData.py
+++ bin/SplitData.py
@@ -8,9 +8,84 @@ Created on Tue Nov 17 15:11:36 2020
 
 import pandas as pd
 import numpy as np
-import os
+from sklearn.model_selection import train_test_split
+
+def data_cleaner(df):
+    data = df.copy()
+    #remove the id, partlybad columns
+    data.drop(["id","partlybad"],1,inplace=True)
+    #set new index using the date column
+    data.set_index(["date"],inplace=True)
+    #add binary 'class2' col for event/non-event
+    data["class2"] = np.where(data["class4"] == "nonevent", "nonevent","event")
+    #arrange to put class2 col at the front
+    cols = ['class2'] + [col for col in data if col != 'class2']
+    data = data[cols]
+    return data
+
+##### New way to generate the data
+
+def data_generator(df):
+    '''
+    input:
+        df: dataframe to split stratified by variable class
+        class: string with variable to use as response and as variable to stratify by
+    output:
+        X_train, y_train: variables and response 60% data 
+        X_val, y_val: variables and response 20% data 
+        X_trainval, y_trainval: variables and response 80% data (for cross validation)
+        X_test, y_test: variables and response 20% data 
+    '''
+    
+    X = df.drop(["class2","class4"], axis=1)
+    y = df['class4']
+    
+    # 60%, 20%, 20%
+    # Generate TrainVal 80% and test 20% 
+    X_trainval, X_test, y_trainval, y_test = train_test_split( X, y, test_size=0.20, random_state=42, stratify=y)
+    
+    # Furthermore split train into Train and Val
+    X_train, X_val, y_train, y_val= train_test_split( X_trainval, y_trainval, test_size=0.25, random_state=42, stratify=y_trainval)
+    
+    return X_train, X_val, X_trainval, X_test, y_train, y_val, y_trainval, y_test
 
 
+def convert_binary(y):
+    '''
+    Parameters
+    ----------
+    y : repsonse variable with 4 classes
+
+    Returns
+    -------
+    y : reponse variable 2 classes
+
+    '''
+    y_bin = np.where(y == "nonevent", "nonevent","event")
+    return y_bin
+
+url = "https://raw.githubusercontent.com/williwilliams3/TermProjectIML/master/data/train.csv"
+df = pd.read_csv(url)
+df = data_cleaner(df)
+
+X_train, X_val, X_trainval, X_test, y_train, y_val, y_trainval, y_test = data_generator(df)
+
+# If needed conversion from 4 classes to binary classes
+y_train_bin = convert_binary(y_train)
+y_val_bin = convert_binary(y_val)
+y_trainval_bin = convert_binary(y_trainval)
+y_test_bin = convert_binary(y_test)
+
+'''
+Count the classes by group
+y_trainval.value_counts(normalize=True)
+y_test.value_counts(normalize=True)
+y_train.value_counts(normalize=True)
+y_test.value_counts(normalize=True)
+'''
+
+
+'''
 os.chdir('/Users/bwilliams/GoogleDrive/UniversityOfHelsinki/Fall2020/IML/TeamProject/TermProjectIML/')
 
 df = pd.read_csv('data/npf_train.csv')
@@ -22,3 +97,21 @@ train, validation, test = \
 train.to_csv('data/train.csv', index=False)
 validation.to_csv('data/validation.csv', index=False)
 test.to_csv('data/test.csv', index=False)
+
+
+
+
+train = data_cleaner(train)
+validation = data_cleaner(validation)
+test = data_cleaner(test)
+
+
+
+train['class2'].value_counts(normalize=True)
+validation['class2'].value_counts(normalize=True)
+test['class2'].value_counts(normalize=True)
+
+train['class4'].value_counts(normalize=True)
+validation['class4'].value_counts(normalize=True)
+test['class4'].value_counts(normalize=True)
+'''
